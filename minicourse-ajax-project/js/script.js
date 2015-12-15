648a738a4e7238426c29977f75990d596c82f3e8
Jenny Zhang <canaantt@gmail.com>
2015-12-14 23:12:45 -0800

    haven't connected with NY times

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -14,6 +14,20 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
+    $("form").submit(function(event) {
+        var $city = $("#city").val();
+        var $street = $("#street").val();
+        var $address = $street.trim() + ", " + $city;
+        console.log("address is :" + $address);
+        var imageTag = '<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + $address + '">';
+        $body.append(imageTag);
+    });
+    
+    //var imageUrl = "http://images.all-free-download.com/images/wallpapers_large/beautiful_flowers_wallpaper_flowers_nature_wallpaper_1304.jpg"
+    &fq=source:("The New York Times");
+    $.getJSON("ajax/test.json", function(data) {
+        console.log(data);
+    });
 
     return false;
 };
