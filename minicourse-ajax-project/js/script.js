874669164e7d8d129f4c181cbce38f4a997b92a3
Jenny Zhang <canaantt@gmail.com>
2015-12-15 23:56:24 -0800

    error handling is done

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -6,6 +6,7 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+    var $searchTerm;
 
     // clear out old data before new request
     $wikiElem.text("");
@@ -17,17 +18,32 @@ function loadData() {
     $("form").submit(function(event) {
         var $city = $("#city").val();
         var $street = $("#street").val();
+        $searchTerm = $city;
         var $address = $street.trim() + ", " + $city;
         console.log("address is :" + $address);
         var imageTag = '<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + $address + '">';
         $body.append(imageTag);
     });
     
-    //var imageUrl = "http://images.all-free-download.com/images/wallpapers_large/beautiful_flowers_wallpaper_flowers_nature_wallpaper_1304.jpg"
-    &fq=source:("The New York Times");
-    $.getJSON("ajax/test.json", function(data) {
-        console.log(data);
+    var $api_key="56dda0abb61e8c41932df7baf8df0e5a:1:73776542";
+
+    var url="http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + $searchTerm + "&api-key=" + $api_key;
+    console.log("url is:" + url);
+  
+    var url=url+"123";
+    $.getJSON(url, function(data){
+        $nytHeaderElem.text("New York Times Articles About " + $searchTerm);
+        var articles = data.response.docs;
+        articles.forEach(function(art){
+            $nytElem.append('<li class="article">' +
+                '<a href="' + art.web_url+'">'+art.headline.main+'</a>'+
+                '<p>' + art.snippet + '</p>' + '</li>');
+        })
+    }).error(function(){
+        $nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
     });
+    
+                
 
     return false;
 };
